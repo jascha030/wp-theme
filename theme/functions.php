ad12527481f6a496f76f54a1255eee13dc572c41
Jascha030 <contact@jaschavanaalst.nl>
2021-09-30 03:02:27 +0200

    Add filter to resolve actual theme root with wordpress get_template_dir functions

--- theme/functions.php
+++ theme/functions.php
@@ -14,3 +14,15 @@ function theme_head(): void
 }
 
 add_action('wps_theme_head', 'theme_head', 10);
+
+/**
+ * Filter to make sure `get_template_directory()` and `get_template_directory_uri()` resolve to the root theme folder
+ * instead of the actual root/theme/ file where the style.css resides.
+ */
+function wps_filter_theme_directory(string $path): string
+{
+    return substr($path, 0, -6);
+}
+
+add_filter('template_directory', 'wps_filter_theme_directory', 10, 1);
+add_filter('template_directory_uri', 'wps_filter_theme_directory', 10, 1);
